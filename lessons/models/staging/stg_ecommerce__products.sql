d0de495daf4a118a2e9136101e3343df877562fc
Lamasutra-analytics <lr.analyticslearning@gmail.com>
2023-11-30 11:34:49 -0700

    Updating staging models to match lesson

--- lessons/models/staging/stg_ecommerce__products.sql
+++ lessons/models/staging/stg_ecommerce__products.sql
@@ -1,16 +1,16 @@
 WITH source AS (
-        SELECT *
-        FROM {{ source('thelook_ecommerce', 'products') }}
+	SELECT *
+
+	FROM {{ source('thelook_ecommerce', 'products') }}
 )
 
 SELECT
-        id AS product_id,
-        cost,
-        category,
-        name,
-        brand,
-        retail_price,
-        department,
-        sku,
-        distribution_center_id
+	-- IDs
+	id AS product_id,
+
+	-- Other columns
+	cost,
+	retail_price,
+	department
+
 FROM source
